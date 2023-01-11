73fdd87976aea608dfe6bf48b1f40c1cbf9491f3
Ariel Suarez <suarezariel566@gmail.com>
2023-01-11 16:09:15 -0300

    add project

---
+++ WebApp_CrystalReport/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace WebApp_CrystalReport.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DownloadReport()
+        {
+            try
+            {
+                var report = new ReportClass();
+                report.FileName = Server.MapPath("/Reportes/Products.rpt");
+                report.Load();
+                var connectionInfo = CrystalReportConnection.GetConnection();
+                TableLogOnInfo logOnInfo = new TableLogOnInfo();
+                Tables tables;
+                tables = report.Database.Tables;
+
+                foreach (Table table in tables)
+                {
+                    logOnInfo = table.LogOnInfo;
+                    logOnInfo.ConnectionInfo = connectionInfo;
+                    table.ApplyLogOnInfo(logOnInfo);
+                }
+                Response.Buffer = false;
+                Response.ClearContent();
+                Response.ClearHeaders();
+
+                Stream stream = report.ExportToStream(ExportFormatType.PortableDocFormat);
+                report.Dispose();
+                report.Close();
+                return new FileStreamResult(stream, "application/pdf");
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+   
+    }
+}
---
+++ WebApp_CrystalReport/CrystalReportConnection.cs
@@ -0,0 +1,17 @@
+﻿
+namespace WebApp_CrystalReport
+{
+    public class CrystalReportConnection
+    {
+        public static CrystalDecisions.Shared.ConnectionInfo GetConnection()
+        {
+            CrystalDecisions.Shared.ConnectionInfo connectionInfo = new CrystalDecisions.Shared.ConnectionInfo();
+
+            connectionInfo.ServerName = @"DESKTOP-5ESG0V0\SQLEXPRESS";
+            connectionInfo.DatabaseName = "AdventureWorks2019";
+            connectionInfo.IntegratedSecurity = true;
+
+            return connectionInfo;
+        }
+    }
+}
